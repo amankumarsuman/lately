fb46281b5c586d28d5e9e762ca0c2364b12533dd
amankumarsuman <amankumarsuman.ssjha143@gmail.com>
2021-03-13 10:17:52 +0530

    login and signup script file

---
+++ script.js
@@ -0,0 +1,63 @@
+function store(){
+
+    var name = document.getElementById('email');
+    var pw = document.getElementById('pw');
+    var lowerCaseLetters = /[a-z]/g;
+    var upperCaseLetters = /[A-Z]/g;
+    var numbers = /[0-9]/g;
+
+    if(name.value.length == 0){
+        alert('Please fill in email');
+
+    }else if(pw.value.length == 0){
+        alert('Please fill in password');
+
+    }else if(name.value.length == 0 && pw.value.length == 0){
+        alert('Please fill in email and password');
+
+    }else if(pw.value.length > 8){
+        alert('Max of 8');
+
+    }else if(!pw.value.match(numbers)){
+        alert('please add 1 number');
+
+    }else if(!pw.value.match(upperCaseLetters)){
+        alert('please add 1 uppercase letter');
+
+    }else if(!pw.value.match(lowerCaseLetters)){
+        alert('please add 1 lovercase letter');
+
+    }else{
+        localStorage.setItem('name', name.value);
+        localStorage.setItem('pw', pw.value);
+        // alert('Your account has been created');
+        window.location.href="./latelylogin.html"
+    }
+}
+
+//checking
+function check(){
+    
+    var storedName = localStorage.getItem('name');
+    var storedPw = localStorage.getItem('pw');
+
+    var userName = document.getElementById('userName');
+    var userPw = document.getElementById('userPw');
+    var userRemember = document.getElementById("rememberMe");
+
+    if(userName.value == storedName && userPw.value == storedPw){
+        alert('You are logged in.');
+        
+        
+        
+       
+    }else{
+        alert('Error on login');
+    }
+}
+// if(userPw == localStorage[userName]){
+//     alert("login successful")
+// }
+// else{
+//     alert("not matched")
+// }
